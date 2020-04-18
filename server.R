3adae60cececfd31bc0ec3cf7252f01ad08265c1
LPapai <k.papai.levente@gmail.com>
2020-04-19 01:25:37 +0200

    new plots

--- server.R
+++ server.R
@@ -12,13 +12,33 @@ library(data.table)
 library(leaflet)
 library(magrittr)
 library(htmltools)
+library(plotly)
+library(forecast)
 
 #FUNCTION: Number formatting and rounding
-formatNum<-function(number){
-    format(round(as.numeric(number),2),nsmall=1, big.mark = " ")
+formatNum<-function(number,precision=2){
+  format(round(as.numeric(number),precision),nsmall=precision, big.mark = " ")
+}
+
+#FUNCTION: hover snippet
+LabelSnippet <- function(country=NA,date=NA,population=NA, cases=NA,
+                         CCases=NA,CDeaths=NA,C.CP=NA,C.DC=NA,
+                         GR=NA,daysSinceFirst=NA){
+  paste(ifelse(is.na(country),'',paste('<b>',country,'</b>')),
+        ifelse(is.na(date),'',paste('(@',date,')')),
+        ifelse(is.na(cases),'',paste('<br>New cases:',formatNum(cases,0))),
+        ifelse(is.na(population),'',paste('<br>Population:',formatNum(population,0))),
+        ifelse(is.na(CCases),'',paste('<br>Cumulative cases:',formatNum(CCases,0))),
+        ifelse(is.na(CDeaths),'',paste('<br>Cumulative deaths:',formatNum(CDeaths,0))),
+        ifelse(is.na(C.CP),'',paste('<br>Cases/1000ppl ratio:',formatNum(C.CP,2))),
+        ifelse(is.na(C.DC),'',paste('<br>Deaths/100 cases ratio:',formatNum(C.DC,2))),
+        ifelse(is.na(GR),'',paste('<br>Growth rate:',formatNum(GR,2)))
+  )
 }
 #Replace vectors
-rc<-c("cases"="Cases","deaths"="Deaths","ratioDC"="Deaths/Cases ratio","ratioCP"="Cases/1000people ratio")
+rc<-c("CCases"="Cumulative cases","CDeaths"="Cumulative deaths",
+      "C.DC"="Deaths/Cases ratio","C.CP"="Cases/1000people ratio",
+      "GrowthRate"="Growth Rate", "pop"="Population","country"="Country")
 
 ###################
 # Getting the dataset
@@ -36,60 +56,126 @@ setnames(countryISO, old = "Longitude..average.", new = "lng")
 data<-data.table(data)
 countryISO<-data.table(countryISO)
 
-# specifying date class
+# Specifying date class
 data$dateRep<-as.Date(data$dateRep, tryFormats = c("%Y-%m-%d", "%d/%m/%Y"))
+setnames(data,"countriesAndTerritories","country")
 
-# Computing cumulative sums and ratios:
-latest<-data[order(dateRep),.(dateRep,cases=cumsum(cases),deaths=cumsum(deaths),
-                              ratioDC=(100*cumsum(deaths)/cumsum(cases)),
-                              ratioCP=(1000*cumsum(cases)/popData2018[1]),
-                              pop=popData2018[1]),
-             keyby=geoId]
+# Growth Rate time frame
+grtu <- 7
+setkey(data,dateRep)
+
+# Computing cumulative data and growth rate
+data<-data[,':='(CCases=cumsum(cases),CDeaths=cumsum(deaths),
+              pop=popData2018[1]),
+        by=country][CCases>10,':='(GrowthRate= (CCases-shift(CCases,n=grtu))/(shift(CCases,n=grtu)-shift(CCases,n=2*grtu)),
+                                             C.DC=100*CDeaths/CCases,
+                                             C.CP=1000*CCases/popData2018[1]), 
+                    by=country][,
+                      days.SinceFirstCase:=dateRep-.SD[CCases>0,.(d=min(dateRep))]$d,
+                      by=country]
 
 # Joining tables
-d <- merge(latest,countryISO[,.(geoId,Country,lat,lng)], by="geoId", all.x=T)
-d <- d[!is.na(Country)]
+d <- merge(data,countryISO[,.(geoId,lat,lng)], by="geoId", all.x=T)
+d <- d[!is.na(geoId)]
 
-#Tooltip labels:
-d$label <- paste0("<b>",d$Country,"</b> (Pop:",formatNum(d$pop),")<br/>",
-                  "No. of cases: ",formatNum(d$cases),"<br/>",
-                  "No. of deaths: ",formatNum(d$deaths),"<br/>",
-                  "Deaths/100 cases ratio: ",formatNum(d$ratioDC),"<br/>",
-                  "Cases/1000ppl ratio: ",formatNum(d$ratioCP),"<br/>")
+# Tooltip labels:
+d$label <- LabelSnippet(country = d$country,population = d$pop,CCases = d$CCases,
+                        CDeaths = d$CDeaths,C.CP=d$C.CP,
+                        C.DC = d$C.DC,
+                        GR=d$GrowthRate)
 
 ###############
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-    dateR<-reactive ({as.Date(input$date)})
-    dataR <- reactive({
-        switch(input$radio,
-               cases={d[dateRep==dateR(),][order(-cases),]},
-               deaths={d[dateRep==dateR(),][order(-deaths),]},
-               ratioDC={d[dateRep==dateR(),][order(-ratioDC),]},
-               ratioCP={d[dateRep==dateR(),][order(-ratioCP),]},
-               {d[dateRep==dateR(),][order(-cases),]})
-    })
+  dateR<-reactive ({as.Date(input$date)})
+  dataR <- reactive({
+    switch(input$radio,
+           CCases={d[dateRep==dateR(),][order(-CCases),]},
+           CDeaths={d[dateRep==dateR(),][order(-CDeaths),]},
+           C.DC={d[dateRep==dateR(),][order(-C.DC),]},
+           C.CP={d[dateRep==dateR(),][order(-C.CP),]},
+           GrowthRate={d[dateRep==dateR(),][order(-GrowthRate),]},
+           {d[dateRep==dateR(),][order(-CCases),]})
+  })
+  
+  ## MAP init.
+  output$map <- renderLeaflet({
+    leaflet(d) %>% 
+      addTiles() %>%
+      fitBounds(lat1=40, lng1=6, lng2 = 8, lat2 = 55)
+  })
+  
+  ## MAP update
+  observe({
+    ## Circle sizer
+    sizer <- switch(input$radio,
+                    C.DC=5E4,
+                    C.CP=1E5,
+                    CCases=900,
+                    CDeaths=1600,
+                    GrowthRate=1E5,
+                    1000
+    )
+    ## Leaflet
+    leafletProxy("map", data = dataR()) %>%
+      clearShapes() %>%
+      addCircles(weight = 0, radius = sqrt(dataR()[[input$radio]])*sizer, fillOpacity=0.4, color="#aa1188", popup = ~label)
+  })
+  
+  ## Table and headers
+  output$table <- renderTable({
+    t<-data.table(dataR()[,c(8,11:15)])
+    setnames(t,old=(names(rc)[names(rc)%in%names(t)]),new=rc[names(rc)%in%names(t)])
+    t})
+  output$h1_1 <- renderText({paste(rc[input$radio], "on", dateR())})
+  output$h1_2 <- renderText({paste(rc[input$radio], "by country on",dateR())})
+  
+  ## navbar: Trajectory
+  updateSelectizeInput(session, 'country', choices = unique(d$country), server = TRUE)
+  output$trajectory <- renderPlotly(({
+    if(is.null(input$country)) return(NULL)
     
-    output$map <- renderLeaflet({
-        leaflet(d) %>% 
-             addTiles() %>%
-             fitBounds(lat1=40, lng1=6, lng2 = 8, lat2 = 55)
-    })
-
-    observe({
-        sizer <- switch(input$radio,
-                        ratioDC={sizer<-5E4},
-                        ratioCP={sizer<-1E5},
-                        cases={sizer<-900},
-                        deaths={sizer<-1600},
-                        {1000}
-        )
-        leafletProxy("map", data = dataR()) %>%
-            clearShapes() %>%
-            addCircles(weight = 0, radius = sqrt(dataR()[[input$radio]])*sizer, fillOpacity=0.4, color="#aa1188", popup = ~label)
-    })
+    d.selected<-d[country %in% input$country,]
     
-    output$table <- renderTable(dataR()[,c(8,3:6)])
-    output$h1_1 <- renderText({paste(rc[input$radio], "until ", dateR())})
-    output$h1_2 <- renderText({paste(rc[input$radio], "by country until ",dateR())})
+    axis.x <- list(title=paste("Cumulative",input$caseordeath),showgrid = FALSE)
+    axis.y <- list(title=paste("New",input$caseordeath),showgrid = FALSE)
+    
+    if(input$log) {
+      axis.x["type"] <- "log"
+      axis.y["type"] <- "log"
+      d.selected$cases<-d.selected$cases+1
+    }
+    if(input$ma) {
+      d.selected$cases <- ma(d.selected$cases,2)
+      d.selected$deaths <- ma(d.selected$deaths,2)
+    }
+    
+    plot_ly(d.selected, x= ~get(ifelse(input$caseordeath=="cases","CCases","CDeaths")), 
+            y = ~get(input$caseordeath), color=~country, hoverinfo='text', mode='lines+markers',
+            sizes = c(1, 10), marker = list(sizemode = 'diameter'),
+            text=~LabelSnippet(country=country,date=dateRep, population=pop, cases=cases,
+                               CCases = CCases, CDeaths = CDeaths,
+                               GR=GrowthRate,
+                               C.DC = C.DC)) %>% 
+      layout(title=paste("Trajectory of COVID-19 Confirmed",input$caseordeath),
+             xaxis=axis.x, yaxis=axis.y) %>% 
+      add_markers(text = ~text, size=~pop, showlegend = FALSE) %>% 
+      add_lines(showlegend = TRUE)
+  }))
+  
+  ## navbar: MORE STAT
+  
+  output$`case-cumcase` <- renderPlotly({
+    plot_ly(d[dateRep==max(dateRep) & CCases>50,][order(-pop),head(.SD,80)], type = 'scatter', mode = 'markers',
+            x=~(CCases/as.numeric(days.SinceFirstCase)), y=~(C.DC), color=~country, size=~C.CP, hoverinfo='text', 
+            sizes = c(10, 70), marker = list(sizemode = 'diameter'),
+            text=~LabelSnippet(country=country,date=dateRep, population=pop,
+                               CCases = CCases, CDeaths = CDeaths,
+                               GR=GrowthRate,
+                               C.DC=C.DC)) %>%
+      layout(title="Relative deaths ~ cases (size=population)",
+             xaxis=list(title="Avarage new cases per day", showgrid = FALSE,type = "log"),
+             yaxis=list(title="Death count per 100 cases",showgrid = FALSE,type = "log"),
+             height = 600)
+  })
 })
--- ui.R
+++ ui.R
@@ -12,26 +12,43 @@ library(shinythemes)
 library(leaflet)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(navbarPage("COVID-19 cases",
   theme = shinytheme("flatly"),
-  # Application title
-  titlePanel("COVID-19 cases around the world"),
-  p("This is a simple map showing some basic statistics regarding the COVID-19 pandemic up until the given date on the slider below.",br(),
-    "The dataset is updated dynamically, and can be accessed from the link below."),
-  p(a("Dataset can be downloaded from here. (.csv)",href="https://opendata.ecdc.europa.eu/covid19/casedistribution/csv")),
-  sidebarLayout(
-    sidebarPanel(
-      radioButtons("radio", "Select parameter to show:",
-                   choices = list("Cumulative case count" = "cases", "Cumulative death count" = "deaths",
-                                  "Deaths/Cases ratio (per 100ppl)" = "ratioDC", "Cases/Population ratio (per 1000ppl)" = "ratioCP"),
-                   selected = "cases"),
-      sliderInput("date", "Select date:",
-                  min = as.Date("2019-12-30"), max = Sys.Date()-1,
-                  value = Sys.Date())),
-    mainPanel(
-      h2(textOutput("h1_1")),
-      leafletOutput("map"),
-      p(),
-      h2(textOutput("h1_2")),
-      tableOutput('table')))
+  tabPanel("Map",
+           # Application title
+           sidebarLayout(
+             sidebarPanel(
+               h3(textOutput("h1_1")),
+               radioButtons("radio", "Select parameter to show:",
+                            choices = list("Cumulative case count" = "CCases", "Cumulative death count" = "CDeaths",
+                                           "Deaths/Cases ratio (per 100ppl)" = "C.DC", "Cases/Population ratio (per 1000ppl)" = "C.CP",
+                                           "Growth Rate"="GrowthRate"),
+                            selected = "CCases"),
+               sliderInput("date", "Select date:",
+                           min = as.Date("2019-12-30"), max = Sys.Date()-1,
+                           value = Sys.Date())),
+             mainPanel(
+               leafletOutput("map"),
+               p(),
+               h2(textOutput("h1_2")),
+               tableOutput('table')))
+  ),
+  tabPanel("Trajectory",
+           selectizeInput("country","Select countries to show", multiple=T,
+                          choices=NULL),
+           selectInput("caseordeath","Select the base of the data",
+                       choices = list("Confirmed cases"="cases","Confirmed deaths"="deaths"), selected = "cases"),
+           checkboxInput("log","Logarithmic scales"),
+           checkboxInput("ma","Moving average (2days)",value=T),
+           plotlyOutput("trajectory")
+  ),
+  tabPanel("Deaths/Cases",
+           column(8,plotlyOutput("case-cumcase"), offset=2)
+  ),
+  tabPanel("About",
+           titlePanel("COVID-19 cases around the world"),
+           p("This is a simple map showing some basic statistics regarding the COVID-19 pandemic up until the given date on the slider below.",br(),
+             "The dataset is updated dynamically, and can be accessed from the link below."),
+           p(a("Dataset can be downloaded from here. (.csv)",href="https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"))
+  )
 ))
